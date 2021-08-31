a18b9363e102c3558bbee0f8853ab40b091372e4
kevin bhavsar <kevinbhavsar567@gmail.com>
2021-08-31 20:10:01 +0530

    add logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Result from "./components/Result";
+import Error from "./components/Error";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import data from "./data.json";
+
+export default class App extends Component {
+  state = {
+    name: "",
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact={true} path="/" component={Main}></Route>
+            <Route path="/result/:name" component={Result}></Route>
+            <Route component={Error}></Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
